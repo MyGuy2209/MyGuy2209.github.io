d9f71d57879d25970847e0200142dfaf7f90ce03
salle <salle2209@gmail.com>
2019-03-15 13:08:45 +0100

    .

---
+++ IT2 Andre semester/Spill Prosjekt/Tetris/Tetris.js
@@ -0,0 +1,10 @@
+const cvs = document.querySelector("#Tetris");
+
+const ctx = cvs.getContext("2d");
+
+const score = document.querySelector("#poeng");
+
+
+const ROW = 20;
+
+const COL = COLUMN= 10;
